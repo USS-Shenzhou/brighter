4c31fc7cc1bba824b23edaf9d3f6f948752b8409
USS-Shenzhou <969840795@qq.com>
2021-12-04 15:19:42 +0800

    trying...

--- src/main/java/cn/ussshenzhou/brighter/mixin/MixinBlockLightEngine.java
+++ src/main/java/cn/ussshenzhou/brighter/mixin/MixinBlockLightEngine.java
@@ -1,118 +1,103 @@
 package cn.ussshenzhou.brighter.mixin;
 
-/*import net.minecraft.block.BlockState;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.MinecraftGame;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.SectionPos;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.LightType;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.IChunkLightProvider;
-import net.minecraft.world.chunk.NibbleArray;
-import net.minecraft.world.lighting.*;
-import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.LogicalSidedProvider;
-import org.apache.commons.lang3.mutable.MutableInt;
+import cn.ussshenzhou.brighter.util.FakeBlockLightSectionStorage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LightChunkGetter;
+import net.minecraft.world.level.lighting.BlockLightEngine;
+import net.minecraft.world.level.lighting.LayerLightEngine;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.apache.logging.log4j.LogManager;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;*/
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.apache.commons.lang3.mutable.MutableInt;
 
 /**
  * @author Tony Yu
  */
-public abstract class MixinBlockLightEngine {
-}
-/*@Mixin(BlockLightEngine.class)
-public abstract class MixinBlockLightEngine extends LightEngine<BlockLightStorage.StorageMap, BlockLightStorage> {
-
-    public MixinBlockLightEngine(IChunkLightProvider chunkLightProvider, LightType lightTypeIn, BlockLightStorage storageIn) {
-        super(chunkLightProvider, lightTypeIn, storageIn);
+@Mixin(BlockLightEngine.class)
+public abstract class MixinBlockLightEngine extends LayerLightEngine<FakeBlockLightSectionStorage.FakeBlockDataLayerStorageMap, FakeBlockLightSectionStorage>{
+    public MixinBlockLightEngine(LightChunkGetter p_75640_, LightLayer p_75641_, FakeBlockLightSectionStorage p_75642_) {
+        super(p_75640_, p_75641_, p_75642_);
     }
-
-    @Inject(method = "computeLevel", at = @At("RETURN"), remap = false, cancellable = true)
-    private void brighterComputeLevel(long pos, long excludedSourcePos, int level, CallbackInfoReturnable<Integer> cir) {
-    }
-
-    @Inject(method = "getEdgeLevel", at = @At("RETURN"), remap = false, cancellable = true)
-    private void brighterGetEdgeLevel(long startPos, long endPos, int startLevel, CallbackInfoReturnable<Integer> cir) {
-        //cir.setReturnValue(-1);
-    }
-
     @Shadow(remap = false)
-    @Final
-    private final BlockPos.Mutable mutablePos = new BlockPos.Mutable();
+    protected abstract int getLightEmission(long p_75509_);
 
-    @Shadow(remap = false)
-    private int getLightValue(long worldPos) {
-        int i = BlockPos.unpackX(worldPos);
-        int j = BlockPos.unpackY(worldPos);
-        int k = BlockPos.unpackZ(worldPos);
-        IBlockReader iblockreader = this.chunkProvider.getChunkForLight(i >> 4, k >> 4);
-        return iblockreader != null ? iblockreader.getLightValue(this.mutablePos.setPos(i, j, k)) : 0;
-    }
-
-    @Inject(method = "getEdgeLevel", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "computeLevelFromNeighbor", at = @At("HEAD"), remap = false, cancellable = true)
     private void brighterGetEdgeLevelHead(long startPos, long endPos, int startLevel, CallbackInfoReturnable<Integer> cir) {
-        if (endPos == Long.MAX_VALUE) {
-            cir.setReturnValue(15);
-        } else if (startPos == Long.MAX_VALUE) {
-            cir.setReturnValue(startLevel + 15 - this.getLightValue(endPos));
-        } else if (startLevel >= 15) {
-            cir.setReturnValue(startLevel);
-        } else {
-            int i = Integer.signum(BlockPos.unpackX(endPos) - BlockPos.unpackX(startPos));
-            int j = Integer.signum(BlockPos.unpackY(endPos) - BlockPos.unpackY(startPos));
-            int k = Integer.signum(BlockPos.unpackZ(endPos) - BlockPos.unpackZ(startPos));
-            Direction direction = Direction.byLong(i, j, k);
-            if (direction == null) {
-                cir.setReturnValue(15);
-            } else {
-                MutableInt mutableint = new MutableInt();
-                BlockState blockstate = this.getBlockAndOpacity(endPos, mutableint);
-                if (mutableint.getValue() >= 15) {
-                    cir.setReturnValue(15);
-                } else {
-                    BlockState blockstate1 = this.getBlockAndOpacity(startPos, (MutableInt) null);
-                    VoxelShape voxelshape = this.getVoxelShape(blockstate1, startPos, direction);
-                    VoxelShape voxelshape1 = this.getVoxelShape(blockstate, endPos, direction.getOpposite());
-
-
-                    int l = 1;
-                    long prevPos = startPos + (startPos - endPos);
-                    long prevLevel = 15;
+        /*
                     try {
                         prevLevel = 15 - ((SectionLightStorageAccessor) storage).callGetLight(prevPos);
                     } catch (NullPointerException ignored) {
                     }
+        }*/
+        if (endPos == Long.MAX_VALUE) {
+            cir.setReturnValue(15);
+        } else if (startPos == Long.MAX_VALUE) {
+            cir.setReturnValue(startLevel + 15 - this.getLightEmission(endPos));
+        } else if (startLevel >= 15) {
+            cir.setReturnValue(startLevel);
+        } else {
+            int i = Integer.signum(BlockPos.getX(endPos) - BlockPos.getX(startPos));
+            int j = Integer.signum(BlockPos.getY(endPos) - BlockPos.getY(startPos));
+            int k = Integer.signum(BlockPos.getZ(endPos) - BlockPos.getZ(startPos));
+            Direction direction = Direction.fromNormal(i, j, k);
+            if (direction == null) {
+                cir.setReturnValue(15);
+            } else {
+                MutableInt mutableint = new MutableInt();
+                BlockState blockstate = this.getStateAndOpacity(endPos, mutableint);
+                if (mutableint.getValue() >= 15) {
+                    cir.setReturnValue(15);
+                } else {
+                    BlockState blockstate1 = this.getStateAndOpacity(startPos, (MutableInt)null);
+                    VoxelShape voxelshape = this.getShape(blockstate1, startPos, direction);
+                    VoxelShape voxelshape1 = this.getShape(blockstate, endPos, direction.getOpposite());
+                    //LogManager.getLogger().warn("!");
+                    int l = 1;
+                    long prevPos = startPos + (startPos - endPos);
+                    long prevLevel = 15;
+                    //prevLevel = this.getLightEmission(prevPos);
+                    prevLevel = this.getLevel(prevPos);
                     if (prevLevel == startLevel - 1 ) {
                         l = 0;
                     }
-
                     l += mutableint.getValue();
 
-                    int a = (int) (Math.random() * 100000);
-                    if (endPos == BlockPos.pack(-39, 4, -124)) {
-                        LogManager.getLogger().info(a + "---" + "prevPos---" + BlockPos.unpackX(prevPos) + "---" + BlockPos.unpackY(prevPos) + "---" + BlockPos.unpackZ(prevPos) + "---" + prevLevel);
-                        LogManager.getLogger().info(a + "---" + "startPos---" + BlockPos.unpackX(startPos) + "---" + BlockPos.unpackY(startPos) + "---" + BlockPos.unpackZ(startPos) + "---" + startLevel);
-                        LogManager.getLogger().info(a + "---" + "endPos---" + BlockPos.unpackX(endPos) + "---" + BlockPos.unpackY(endPos) + "---" + BlockPos.unpackZ(endPos) + "---" + (startLevel + l));
-                    }
-
-                    cir.setReturnValue(
-                            VoxelShapes.faceShapeCovers(voxelshape, voxelshape1) ? 15 :
-                                    startLevel + l
+                    cir.setReturnValue(Shapes.faceShapeOccludes(voxelshape, voxelshape1) ? 15
+                            : startLevel + l
                             //Math.max(1, mutableint.getValue())
                     );
                 }
             }
         }
     }
-}*/
+
+    @Inject(method = "getComputedLevel", at = @At("RETURN"), remap = false, cancellable = true)
+    private void brighterGetComputedLevel(long pos, long excludedSourcePos, int level, CallbackInfoReturnable<Integer> cir) {
+        /*if (Minecraft.getInstance().isSameThread()){
+            LogManager.getLogger().info(
+                    BlockPos.getX(pos)
+                            + " "
+                            + BlockPos.getY(pos)
+                            + " "
+                            + BlockPos.getZ(pos)
+                            + "  |  "
+                            + BlockPos.getX(excludedSourcePos)
+                            + " "
+                            + BlockPos.getY(excludedSourcePos)
+                            + " "
+                            + BlockPos.getZ(excludedSourcePos)
+                            + "   "
+                            + (15-level)
+                            + "   "
+                            + (15-cir.getReturnValue()));
+        }*/
+    }
+}
