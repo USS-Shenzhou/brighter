a079bbb9e7e573a0abfbe64ea6541f6ccf1b38ee
USS-Shenzhou <969840795@qq.com>
2021-12-10 12:29:45 +0800

    incompatible with starlight

--- src/main/java/cn/usssehznhou/brighterf/BrighterF.java
+++ src/main/java/cn/usssehznhou/brighterf/BrighterF.java
@@ -1,6 +1,8 @@
 package cn.usssehznhou.brighterf;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * @author Tony Yu
@@ -8,6 +10,8 @@ import net.fabricmc.api.ModInitializer;
 public class BrighterF implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        if (FabricLoader.getInstance().isModLoaded("starlight")){
+            LogManager.getLogger().error("BrighterF is noy compatible with Starlight! BrighterF will not work.");
+        }
     }
 }
