03e812daa1150c909416085b6d7978d16a0c7915
USS-Shenzhou <969840795@qq.com>
2022-09-27 22:33:32 +0800

    update to 1.19.2

--- src/main/java/cn/ussshenzhou/brighter/Brighter.java
+++ src/main/java/cn/ussshenzhou/brighter/Brighter.java
@@ -1,15 +1,13 @@
 package cn.ussshenzhou.brighter;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forgespi.language.IModInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,7 +27,7 @@ public class Brighter {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -41,6 +39,20 @@ public class Brighter {
 
         if (ModList.get().isLoaded("starlight")) {
             LogManager.getLogger().error("Brighter is noy compatible with Starlight! Brighter will not work.");
+        }
+    }
+
+    private void setupClient(final FMLClientSetupEvent event){
+        if (ModList.get().isLoaded("starlight")) {
+            IModInfo iModInfo = ModList.get().getModContainerById("brighter")
+                    .map(ModContainer::getModInfo)
+                    .orElseThrow(() -> new IllegalStateException("ModInfo absent while mod itself is present"));
+
+            ModLoader.get().addWarning(new ModLoadingWarning(
+                    iModInfo,
+                    ModLoadingStage.SIDED_SETUP,
+                    "Brighter is noy compatible with Starlight! Brighter will not work."
+            ));
         }
     }
 
@@ -64,16 +76,5 @@ public class Brighter {
     public void onServerStarting(FMLDedicatedServerSetupEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
