8d6d93ddb7a6a79085750195f7c0aae111b48ccb
USS-Shenzhou <969840795@qq.com>
2023-08-25 16:42:50 +0800

    use Overwrite instead of Inject to avoid new CallBackInfo instance

--- src/main/java/cn/ussshenzhou/brighter/mixin/MixinBlockLightEngine.java
+++ src/main/java/cn/ussshenzhou/brighter/mixin/MixinBlockLightEngine.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.lighting.BlockLightSectionStorage;
 import net.minecraft.world.level.lighting.LightEngine;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -30,8 +31,12 @@ public abstract class MixinBlockLightEngine extends LightEngine<BlockLightSectio
     @Shadow
     private BlockPos.MutableBlockPos mutablePos;
 
-    @Inject(method = "propagateIncrease", at = @At("HEAD"), cancellable = true)
-    private void brighterPropagateIncrease(long startPos, long noIdeaWhatThisFuckIs, int startLevel, CallbackInfo ci) {
+    /**
+     * @author Brighter by USS_Shenzhou
+     * @reason use Overwrite instead of Inject to avoid new CallBackInfo instance.
+     */
+    @Overwrite
+    public void propagateIncrease(long startPos, long noIdeaWhatThisFuckIs, int startLevel) {
         BlockState blockstate = null;
 
         for (Direction direction : PROPAGATION_DIRECTIONS) {
@@ -71,15 +76,17 @@ public abstract class MixinBlockLightEngine extends LightEngine<BlockLightSectio
                 }
             }
         }
-
-        ci.cancel();
     }
 
     @Shadow
     protected abstract int getEmission(long p_285243_, BlockState p_284973_);
 
-    @Inject(method = "propagateDecrease", at = @At("HEAD"), cancellable = true)
-    private void brighterPropagateDecrease(long startPos, long noIdeaWhatThisFuckIs, CallbackInfo ci) {
+    /**
+     * @author Brighter by USS_Shenzhou
+     * @reason use Overwrite instead of Inject to avoid new CallBackInfo instance.
+     */
+    @Overwrite
+    public void propagateDecrease(long startPos, long noIdeaWhatThisFuckIs) {
         int i = QueueEntry.getFromLevel(noIdeaWhatThisFuckIs);
 
         for (Direction direction : PROPAGATION_DIRECTIONS) {
@@ -109,9 +116,6 @@ public abstract class MixinBlockLightEngine extends LightEngine<BlockLightSectio
                 }
             }
         }
-
-
-        ci.cancel();
     }
 
 
